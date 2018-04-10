fde6f0313556349a3b1e85fb7b95998a5a996bb5
yatinghu <1692680311@qq.com>
2018-04-10 18:01:41 +0800

    111

---
+++ js/common.js
@@ -0,0 +1,11 @@
+mui.init({
+  gestureConfig:{
+   tap: true, //默认为true
+   doubletap: true, //默认为false
+   longtap: true, //默认为false
+   swipe: true, //默认为true
+   drag: true, //默认为true
+   hold:false,//默认为false，不监听
+   release:false//默认为false，不监听
+  }
+});
